4f778ff011f7663495a8edc962ec433b99b0a654
KostyaSkorik <endatedd@student.21-school.ru>
2025-07-04 13:26:50 +0300

    upgrade stepa status

--- src/main/java/by/kostya/megachat/rest/UserController.java
+++ src/main/java/by/kostya/megachat/rest/UserController.java
@@ -18,9 +18,9 @@ public class UserController {
     public ResponseEntity<List<User>> getAll(){
         List<User> users = new ArrayList<>();
 
-        users.add(new User(1L,"Stepa","Loh"));
-        users.add(new User(2L,"Stepa","Loh"));
-        users.add(new User(3L,"Stepa","Loh"));
+        users.add(new User(1L,"Stepa","cool gay"));
+        users.add(new User(2L,"Stepa","cool gay"));
+        users.add(new User(3L,"Stepa","cool gay"));
 
         return ResponseEntity.ok().body(users);
     }
